5222a5c1ec8de665c1694d8377af28879b85f0a8
Raihan Yudi Syukma <raihanys03@gmail.com>
2025-05-27 22:34:57 +0700

    Update AIController.php
    Add more features about the virtual assistant.

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -15,7 +15,7 @@ class AIController extends Controller
         ])->post('https://api.groq.com/openai/v1/chat/completions', [
             'model' => 'llama3-70b-8192',
             'messages' => [
-                ['role' => 'system', 'content' => 'You are Rainy, a helpful assistant.'],
+                ['role' => 'system', 'content' => "You are Rainy, a helpful assistant. You can speak English and Indonesia. Your owner's name is Raihan Yudi Syukma. He is a fullstack web developer that creating web applications using frameworks such as React, CodeIgniter and Laravel. He uses MySQL as database."],
                 ['role' => 'user', 'content' => $request->input('message')],
             ],
             'temperature' => 0.7,
