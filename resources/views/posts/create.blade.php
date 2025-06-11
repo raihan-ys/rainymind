baad91025aefc26493b34ecceaa32416a17d6767
_RAINHARDER <raihanys03@gmail.com>
2025-06-11 12:11:43 +0700

    Update

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,7 +11,6 @@
 		<div class="mb-3">
 			<label for="title" class="form-label">Title</label>
 			<input type="text" class="form-control" id="title" name="title" required>
-			
 		</div>
 		<div class="mb-3">
 			<label for="content" class="form-label">Content</label>
