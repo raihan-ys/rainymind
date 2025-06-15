0eae9a96ef3e0cf8b70063c08549beb2a1ecd475
_RAINHARDER <raihanys03@gmail.com>
2025-06-15 17:33:19 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URI', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
